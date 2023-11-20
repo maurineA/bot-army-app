560343261a6b03bdcc2eba945050a21d3ba6e5b8
Mwambui123 <maurinawambuia@gmail.com>
2023-11-20 13:37:20 +0300

    display the data on the DOM

---
+++ development/code/phase2/bot-army-app/src/Component/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import BotCollection from './BotCollection';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <BotCollection />
+    </div>
+  );
+}
+
+export default App;
--- development/code/phase2/bot-army-app/src/Component/BotCollection.js
+++ development/code/phase2/bot-army-app/src/Component/BotCollection.js
@@ -1,18 +1,52 @@
-import React from "react";
+// BotCollection.js
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const BotsCollection = ({enlistBot}) => {
- 
-  
-  }
+
+const BotCollection = () => {
+    
+        const [bots, setBots] = useState([]);
+      
+      
+        useEffect(() =>{
+          fetch('http://localhost:3000/bots')
+          .then((res)=>res.json())
+          .then(data =>{
+            setBots(data)
+          })
+        },[])
+        
 
   return (
+    
     <div>
-        <SortBar />
+      <h2>Bot Collection</h2>
+      <ul>
         {bots.map((bot) => (
-        <BotCard key={bot.id} bot={bot} enlistBot={enlistBot} />
-      ))}
+          <li key={bot.id}>
+            <h3>{bot.name}</h3>
+            <p>{bot.description}</p>
+            <p>Health: {bot.health}</p>
+            <p>Damage: {bot.damage}</p>
+            <p>Armor: {bot.armor}</p>
+            <p>Class: {bot.bot_class}</p>
+            <img src={bot.avatar_url} alt={`${bot.name} Avatar`} />
+            <Link to={`/bot-specs/${bot.id}`}>
+              <button>View Details</button>
+            </Link>
+            <button onClick={() => onBotClick(bot)}>Add to Army</button>
+         
+
+           
+            {/* <button onClick={() => enlistBot(bot)}>Enlist Bot</button> */}
+           
+          </li>
+        ))}
+      </ul>
+      
     </div>
+    
   );
+};
 
-
-export default BotsCollection;
+export default BotCollection;
